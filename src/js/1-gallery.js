4654eb51ab625173cec10b4d3914be03da8f30fc
julia <julia.zhyvylo@asila.mk>
2024-11-29 19:12:55 +0100

    task 1 done

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,22 +1,21 @@
-import SimpleLightbox from "simplelightbox";
-import images from "./images-array";
+import SimpleLightbox from 'simplelightbox';
+import images from './images-array';
 
-// Додатковий імпорт стилів
-import "simplelightbox/dist/simple-lightbox.min.css";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const gallery = document.querySelector(".gallery");
+const galleryRef = document.querySelector('.gallery');
 
-const createGalleryItems = (img) => {
+const createGalleryItems = img => {
   return img.map(({ preview, original, description }) => {
-    const liElem = document.createElement("li");
-    liElem.classList.add("gallery-item");
+    const liElem = document.createElement('li');
+    liElem.classList.add('gallery-item');
 
-    const linkElem = document.createElement("a");
-    linkElem.classList.add("gallery-link");
+    const linkElem = document.createElement('a');
+    linkElem.classList.add('gallery-link');
     linkElem.href = original;
 
-    const imgElem = document.createElement("img");
-    imgElem.classList.add("gallery-image");
+    const imgElem = document.createElement('img');
+    imgElem.classList.add('gallery-image');
     imgElem.src = preview;
     imgElem.alt = description;
 
@@ -27,14 +26,15 @@ const createGalleryItems = (img) => {
   });
 };
 
-const appendGalleryItems = (items) => {
-  gallery.append(...items);
+const appendGalleryItems = items => {
+  galleryRef.append(...items);
 };
 
 const items = createGalleryItems(images);
 appendGalleryItems(items);
 
-let glr = new SimpleLightbox('.gallery .gallery-link');
-glr.on('show.simplelightbox', function () {
-	// Do something…
+let gallery = new SimpleLightbox('.gallery .gallery-link', {
+  captionsData: 'alt',
+  captionDelay: 250,
 });
+gallery.on('show.simplelightbox', {});
